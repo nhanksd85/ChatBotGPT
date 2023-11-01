24bfeb3136dc6e71f3ba78879a3652667d479f55
MSI\KAI <nhanksd85@gmail.com>
2023-11-01 15:21:00 +0700

    Init the first project

---
+++ ChatBotGPT/ChatBotGPT/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ChatBotGPT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richOuput = new RichTextBox();
+            txtInput = new TextBox();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // richOuput
+            // 
+            richOuput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richOuput.Location = new Point(12, 12);
+            richOuput.Name = "richOuput";
+            richOuput.Size = new Size(717, 353);
+            richOuput.TabIndex = 0;
+            richOuput.Text = "";
+            // 
+            // txtInput
+            // 
+            txtInput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtInput.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            txtInput.Location = new Point(12, 371);
+            txtInput.Multiline = true;
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(593, 67);
+            txtInput.TabIndex = 1;
+            txtInput.KeyDown += txtInput_KeyDown;
+            // 
+            // btnSend
+            // 
+            btnSend.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            btnSend.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSend.Location = new Point(615, 371);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(114, 67);
+            btnSend.TabIndex = 2;
+            btnSend.Text = "Gửi";
+            btnSend.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(741, 450);
+            Controls.Add(btnSend);
+            Controls.Add(txtInput);
+            Controls.Add(richOuput);
+            Name = "Form1";
+            Text = "ChatBotGPT";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richOuput;
+        private TextBox txtInput;
+        private Button btnSend;
+    }
+}
---
+++ ChatBotGPT/ChatBotGPT/Form1.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Net;
+using System.IO;
+using System.Linq.Expressions;
+using System.Text.Json;
+using System.Security.Policy;
+
+namespace ChatBotGPT
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string getGPTResponse(string input)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                string sURL = "http://lpnserver.net:51087/chat?c=" + input;
+
+                var responce = client.GetStringAsync(sURL).Result;
+                return responce.ToString();
+            }
+            catch (Exception ex)
+            {
+                return "Xin lỗi tôi không hiểu câu hỏi này.";
+            }
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                richOuput.Text += "Hỏi: " + txtInput.Text.Replace(Environment.NewLine, "") + Environment.NewLine;
+               
+
+
+                txtInput.Text = "Đang xử lý ...";
+
+                string answer = getGPTResponse(txtInput.Text);
+               
+                richOuput.Text += "Trả lời: " + answer + Environment.NewLine;
+
+                txtInput.Text = "";
+                txtInput.Focus();
+                
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richOuput.Text = "";
+            txtInput.Focus();
+        }
+    }
+}
